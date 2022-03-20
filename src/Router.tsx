705a466d601f24ce5c578169ab2cf055391c9eb4
kaydan95 <kyori0515@gmail.com>
2022-03-21 00:55:03 +0900

    Update Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -20,7 +20,7 @@ const ChildWrapper = styled.div`
 
 function Router() {
     return (
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter basename="crypto_tracker">
         <ParentsWrapper>
             <ChildWrapper>
                 <Coins/>
