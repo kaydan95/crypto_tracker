c1284f20c1f737c929e83fb47428f60d90e344fd
kaydan95 <kyori0515@gmail.com>
2022-03-21 00:49:45 +0900

    :heavy_plus_sign: update BrowserRouter
    add basename

--- src/Router.tsx
+++ src/Router.tsx
@@ -20,7 +20,7 @@ const ChildWrapper = styled.div`
 
 function Router() {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
         <ParentsWrapper>
             <ChildWrapper>
                 <Coins/>
