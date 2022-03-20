1c93b007652f9c1870a85e5821106c7b869feba0
kaydan95 <kyori0515@gmail.com>
2022-03-21 01:08:15 +0900

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,19 +1,19 @@
-const BASE_URL = `https://api.coinpaprika.com/v1`
+const URL = `https://api.coinpaprika.com/v1`
 
 export function fetchCoins(){
-    return fetch(`${BASE_URL}/coins`).then((response) =>
+    return fetch(`${URL}/coins`).then((response) =>
         response.json()
     );
 }
 
 export function fetchCoinInfo(coinID:string){
-    return fetch(`${BASE_URL}/coins/${coinID}`).then((response) =>
+    return fetch(`${URL}/coins/${coinID}`).then((response) =>
         response.json()
     );
 }
 
 export function fetchCoinTickers(coinID:string){
-    return fetch(`${BASE_URL}/tickers/${coinID}`).then((response) =>
+    return fetch(`${URL}/tickers/${coinID}`).then((response) =>
         response.json()
     );
 }
@@ -21,7 +21,7 @@ export function fetchCoinTickers(coinID:string){
 export function fetchCoinHistory(coinID:string){
     const endDate = Math.floor( Date.now() / 1000);
     const startDate = endDate - 60 * 60 * 24 * 7 * 2;
-    return fetch(`${BASE_URL}/coins/${coinID}/ohlcv/historical?start=${startDate}&end=${endDate}`).then((response) =>
+    return fetch(`${URL}/coins/${coinID}/ohlcv/historical?start=${startDate}&end=${endDate}`).then((response) =>
     response.json()
 );
 }
